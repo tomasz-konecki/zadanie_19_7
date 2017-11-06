5d7ce0d9369b82d2af310297e59fcbce9f713d7d
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-11-06 16:32:50 +0100

    Update code

--- redux-demo/src/CommentForm.js
+++ redux-demo/src/CommentForm.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import './CommentForm.css';
 
-const CommentForm = add => (
-    <form>
-        <div>
-            <label htmlFor="new_comment">Add a comment:</label>
-            <textarea id="new_comment" name="new_comment" rows="6"></textarea>
-        </div>
-        <div>
-            <button className="btn-add" onClick={(e) => {e.preventDefault();
-                const form = document.getElementById("new_comment");
-                add.addComment(form.value);
-                form.value = "";}}>
-            Submit</button>
-        </div>
-    </form>
-)
+class CommentForm extends React.Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            value: ''
+        };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value});
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (this.state.value) {
+            this.props.addComment(this.state.value);
+        }
+        this.setState({value: ''});
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label htmlFor="new_comment">Add a comment:</label>
+                    <textarea id="new_comment" name="new_comment" rows="6" 
+                        onChange={this.handleChange} value={this.state.value} />
+                </div>
+                <div>
+                    <input type="Submit" value="Submit" />
+                </div>
+            </form>
+        )
+    }
+
+}
 
 export default CommentForm;
